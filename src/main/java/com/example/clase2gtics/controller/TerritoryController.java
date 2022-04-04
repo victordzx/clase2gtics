121f1a4713db51887a6e8839f03228c8a00b35b2
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-04-04 09:10:03 -0500

    Cambios mínimos

--- src/main/java/com/example/clase2gtics/controller/TerritoryController.java
+++ src/main/java/com/example/clase2gtics/controller/TerritoryController.java
@@ -7,7 +7,6 @@ import com.example.clase2gtics.repository.TerritoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
